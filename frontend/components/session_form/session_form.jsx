b15e3930631df1cf23558b3d105f03576b8c1b38
frankie siino <frankie.siino@gmail.com>
2019-07-04 11:51:26 -0700

    Begin profile icon appearing when logged in

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -22,7 +22,8 @@ class SessionForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     const user = Object.assign({}, this.state);
-    this.props.processForm(user).then(this.props.closeModal);
+    this.props.processForm(user)
+      .then(this.props.closeModal);
   }
 
   handleDemoUser(e) {
