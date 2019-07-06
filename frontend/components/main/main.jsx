a43d4ad0c4b52ea8df4135b2c7d4089dd21d6a4f
frankie siino <frankie.siino@gmail.com>
2019-07-05 23:06:59 -0700

    Added subsection just above footer to splash page

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -68,7 +68,10 @@ class Main extends React.Component {
           </div>
 
           <div className="wires-container">
-
+            <h3 className="wires-header">The car that pays for itself</h3>
+            <p className="wires-text">Make a dent in your monthly car payments — on average, Turo hosts can cover their payments by sharing their cars just nine days per month.</p>
+            <br/><br/>  
+            <p className="wires-text">You’re covered with up to $1 million in liability insurance, and your car is contractually protected against theft and physical damage. Or bring your own commercial rental insurance and take a bigger piece of the pie.*</p>
           </div>
 
 
