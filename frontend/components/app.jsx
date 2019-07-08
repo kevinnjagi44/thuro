411e450ae0ebb536cd7ca93a2ed4a9a19fb02113
frankie siino <frankie.siino@gmail.com>
2019-07-07 19:27:34 -0700

    Add authroute to list car

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBarContainer from './navbar/navbar_container';
 import { Route } from 'react-router-dom';
-import { AuthRoute } from '../util/route_util';
+import { AuthRoute, ProtectedRoute  } from '../util/route_util';
 import LogInFormContainer from './session_form/login_form_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import { Link, Switch } from 'react-router-dom';
@@ -25,6 +25,7 @@ const App = () => (
       <AuthRoute path ="/login" component={LogInFormContainer} />
       <AuthRoute path ="/signup" component={SignUpFormContainer} />
       <Route exact path="/cars" component={CarIndexContainer} />
+      {/* <ProtectedRoute exact path="/cars/new" component={CarCreateContainer} /> */}
       <Route exact path="/cars/new" component={CarCreateContainer} />
       <Route path="/" component={Main} />
     </Switch>
