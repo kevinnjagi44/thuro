5dbc807f7de38236083560a2700efb7c1c8e0aee
frankie siino <frankie.siino@gmail.com>
2019-07-11 22:49:02 -0700

    Adjust navbar to open modal if user clicks list car while logged out

--- frontend/components/navbar/navbar.jsx
+++ frontend/components/navbar/navbar.jsx
@@ -10,9 +10,10 @@ const NavBar = ({ currentUser, logout, openModal }) => {
         <button className="navbutton">Search for cars</button>
       </Link>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-      <Link to="/cars/new">
-        <button className="navbutton">List your car</button>
-      </Link>
+      {/* <Link to="/cars/new"> */}
+        {/* <button className="navbutton">List your car</button> */}
+        <button className="navbutton" onClick={() => openModal('login')}>List your car</button>
+      {/* </Link> */}
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <button className="navbutton" onClick={() => openModal('login')}>Login</button>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
