bbb657258e44acb908c2c122836fb188538dcc33
frankie siino <frankie.siino@gmail.com>
2019-08-17 13:48:40 -0700

    Add car and rental data to rental index items

--- frontend/components/rentals/rental_index_item.jsx
+++ frontend/components/rentals/rental_index_item.jsx
@@ -13,26 +13,40 @@ class RentalIndexItem extends React.Component {
   }
 
   render() {
+
     const { rental } = this.props;
 
-    const { car } = this.props;
+    const { car } = this.props.rental;
+
+    const GetDate = (date) => {
+      let newDate = new Date(date);
+      return `${newDate.getMonth()}/${newDate.getDay()}/${newDate.getYear()}`;
+    };
+
+    const CalcDays = (end, start) => {
+      let numDays = Math.abs((new Date(end) - new Date(start)) / 3600000) / 24;
+
+      return numDays;
+    };
 
     return (
-
       <>
         <CarIndexItem 
           car={rental.car}
         />
         <div className="rental-index-tile-details">
-          status
+          {rental.status.slice(0, 1).toUpperCase() + rental.status.slice(1)}
           <br/>
-          startdate - end date
+          {GetDate(rental.start_date)} - {GetDate(rental.end_date)}
           <br/>
-          X days
+          {(() => {
+            let days = CalcDays(rental.end_date, rental.start_date);
+            return days > 1 ? `${days} days` : `${days} day`;
+          })()}
           <br/>
-          price per day x # of days
+          ${car.rate} per day
           <br/>
-          total
+          Total: ${car.rate * CalcDays(rental.end_date, rental.start_date)}
         </div>
         <div className="rental-index-tile-details">
           <button onClick={() => { if (window.confirm('Are you sure you wish to cancel this rental?')) {this.handleDelete(rental.id)} } }>Cancel Rental</button>
